c75996aaa036399186c9cb747ae955e1c98362fc
Francisco Javier Briceño <fjavier.cgam@gmail.com>
2019-11-28 17:26:46 -0600

    Formulario incompleto.
    Se crea parte del formulario de Evento.

--- src/main/java/ni/org/jug/backbean/EventoBackBean.java
+++ src/main/java/ni/org/jug/backbean/EventoBackBean.java
@@ -18,18 +18,28 @@ public class EventoBackBean implements Serializable {
 	private static final long serialVersionUID = 7338777711614870198L;
 	
 	private List<Evento> eventos = new ArrayList<Evento>();
+	private Evento evento = new Evento();
 	
 	@PostConstruct
 	public void init() {
 		Evento evento = new Evento();
 		evento.setId(1);
 		evento.setNombre("Aprendamos a Desarrollar aplicaciones Web con Java");
-		evento.setDescripcion("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+		evento.setDescripcion("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
 		evento.setPasivo(false);
 		evento.setFecha(LocalDate.now());
 		this.getEventos().add(evento);
 	}
 	
+	public Evento getEvento() {
+		return evento;
+	}
+
+	public void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+
+
 
 	public List<Evento> getEventos() {
 		return eventos;
